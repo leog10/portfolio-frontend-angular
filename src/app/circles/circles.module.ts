b5e16f8806b9ecf4786b4d95f29de52ed7e822d2
Leo <leonardogilli@outlook.com>
2022-02-18 18:33:40 -0300

    Agrega modulo y componente principal circles

---
+++ src/app/circles/circles.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CirclesComponent } from './circles.component';
+
+
+
+@NgModule({
+  declarations: [
+    CirclesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CirclesModule { }
