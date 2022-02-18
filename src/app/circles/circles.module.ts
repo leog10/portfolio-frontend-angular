8cbde11bbca5a744de4f1e40e39fc14035795af6
Leo <leonardogilli@outlook.com>
2022-02-18 18:35:45 -0300

    Agrega componentes circle y start-circles

--- src/app/circles/circles.module.ts
+++ src/app/circles/circles.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CirclesComponent } from './circles.component';
+import { StartCirclesComponent } from './start-circles/start-circles.component';
+import { CircleComponent } from './circle/circle.component';
 
 
 
 @NgModule({
   declarations: [
-    CirclesComponent
+    CirclesComponent,
+    StartCirclesComponent,
+    CircleComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/circles/start-circles/start-circles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-start-circles',
+  templateUrl: './start-circles.component.html',
+  styleUrls: ['./start-circles.component.scss']
+})
+export class StartCirclesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
