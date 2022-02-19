3ef116e27b4a81f160b4f0be77f0defd1c17d6dc
Leo <leonardogilli@outlook.com>
2022-02-18 23:31:02 -0300

    Agrega rutas y componentes principales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CirclesModule } from './circles/circles.module';
+import { EditComponent } from './routes/edit/edit.component';
+import { PortfolioComponent } from './routes/portfolio/portfolio.component';
+import { LoginComponent } from './routes/login/login.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { MyProjectsComponent } from './components/my-projects/my-projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditComponent,
+    PortfolioComponent,
+    LoginComponent,
+    AboutMeComponent,
+    EducationComponent,
+    ExperienceComponent,
+    FooterComponent,
+    HeaderComponent,
+    HeroComponent,
+    SkillsComponent,
+    MyProjectsComponent
   ],
   imports: [
     BrowserModule,
