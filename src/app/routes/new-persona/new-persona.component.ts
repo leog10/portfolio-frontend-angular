93e88e1e95431a49381109ab604b58ae1de82b8f
Leo <leonardogilli@outlook.com>
2022-03-12 20:47:01 -0300

    Modifica ingreso fecha nacimiento

--- src/app/routes/new-persona/new-persona.component.ts
+++ src/app/routes/new-persona/new-persona.component.ts
@@ -15,10 +15,6 @@ export class NewPersonaComponent implements OnInit {
 
   hasProfile!: boolean;
 
-  day: number = new Date().getDate();
-  month: number = new Date().getMonth() + 1;
-  year: number = new Date().getFullYear();
-
   persona!: Persona;
 
   firstName!: string;
@@ -46,28 +42,23 @@ export class NewPersonaComponent implements OnInit {
         }
       },
       error: err => {
-        console.log(err)
+        console.log('Error: ',err.error.mensaje)
         }
       });    
   }
 
   onCreate(): void {
-    let _day = this.day.toString();
-    if (_day.length < 2) {
-      _day = `0${this.day}`
-    }
-    let _month = this.month.toString();
-    if (_month.length < 2) {
-      _month = `0${this.month}`
-    }
-    this.birthday = _day + '-' + _month + '-' + this.year;
     this.persona = new Persona(this.firstName, this.lastName, this.position, this.location, this.aboutMeNew, this.birthday, this.backImg, this.profileImg);
     this.personaService.create(this.persona).subscribe({
       next: () => {
         this.router.navigate([`/edit/${this.username}`]);
       },
       error: err => {
-        console.log(err);
+        if (err.error.mensaje === 'Fields (firstName, lastName) should not be empty') {
+          console.log('Error: ',err.error.mensaje);
+        } else {
+          console.log('Error: Date invalid');
+        }        
       }
     });
   }
